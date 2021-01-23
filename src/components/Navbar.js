ad7288fea99035b99167e7097a76755d1f1fe5d7
Gabriel Smith <gabesmithp@gmail.com>
2021-01-23 17:45:40 -0500

    ADD: custom cursors

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,10 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className="navbar">
+    <div className="title">Gabriel Smith</div>
+    <div className="links">
+      
+    </div>
       
     </div>
   )
