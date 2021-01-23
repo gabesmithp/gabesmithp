e458b139621f9d333858946f50752c067bcf3e66
Gabriel Smith <gabesmithp@gmail.com>
2021-01-23 12:58:41 -0500

    ADD: NavBar

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import blobs from '../blobs.png'
+
+const Main = () => {
+  return (
+    <div>
+      <img className="blobs" src={blobs} alt="blobs"/>
+    </div>
+  )
+}
+
+export default Main
